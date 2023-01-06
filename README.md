6b01cab2b231c1dca72543cdace3ef10a9c5917f
pjanks <johnny.k.cassidy@gmail.com>
2023-01-06 09:03:49 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# File Naming App ***Windows Only***
+
+This app can be used to select one or more files from a file explorer or drag and drop one or multiple files, and prepend them with a descriptor. It will also create a folder to store the groups in. **This utility will NOT edit the original files**. I advise checking to make sure the prepended files are not corrupted before erasing original files. After filling out the form and selecting the photos, simply click Submit. Go to the uploads directory of this project and verify that all of your files are there and that they are not corrupted. Node must be installed to run this app.
+
+## Instructions for Starting App
+
+  - clone down this repository
+  - `cd` into project directory
+  - run `node server.js`
+  - go to `127.0.0.1:3001` in your web browser
+
+## Alternitavely
+
+  - clone down this repository
+  - navigate to the project in your file explorer
+  - double-click the `start.bat` file
+
+After you have finished, navigate to the terminal window that opened when the app started and exit the session by pressing `ctrl + c`
---
+++ public/scripts/scripts.js
@@ -0,0 +1,130 @@
+const formData = new FormData();
+let fileOrFiles = [];
+
+const fileDropper = $('.file-dropper');
+const fileDropperForm = $('.file-dropper-form');
+const fileDropperLabel = $('.file-dropper-label');
+
+const handleFileDrop = async (e) => {
+  e.preventDefault();
+  if (e.type === 'drop') {
+    fileOrFiles = e.originalEvent.dataTransfer.files;
+    if (fileOrFiles.length > 1) {
+      const keys = Object.keys(fileOrFiles);
+      keys.forEach(key => {
+        formData.append('files', fileOrFiles[key]);
+      });
+    } else {
+      formData.append('file', fileOrFiles[0]);
+    }
+  }
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const newName = $('.new-name-input').val().replace(/\s+/g, '_');
+  const directory = $('.directory-input').val();
+  
+  if (fileDropper.prop('files').length === 1) {
+
+    formData.append('files', fileDropper.prop('files')[0]);
+
+    const options = {
+      method: 'POST',
+      body: formData,
+    };
+
+    try {
+    const response = await fetch(`http://127.0.0.1:3001/rename_one/${newName}/${directory}`, options);
+    console.log({ response })
+    $('.success-modal').removeClass('hidden');
+    setTimeout(() => {
+      window.location.reload();
+    }, 1500);
+    } catch (err) {
+      alert(`there was an error: ${err}, ${err.message}`);
+    }
+
+  } else if (fileDropper.prop('files').length > 1) {
+
+    const keys = Object.keys(fileDropper.prop('files'));
+    keys.forEach(key => {
+      formData.append('files', fileDropper.prop('files')[key]);
+    });
+
+    const options = {
+      method: 'POST',
+      body: formData,
+    };
+
+    try {
+      const response = await fetch(`http://127.0.0.1:3001/rename_multiple/${newName}/${directory}`, options);
+      console.log({ response });
+      $('.success-modal').removeClass('hidden');
+      setTimeout(() => {
+        window.location.reload();
+      }, 1500);
+    } catch (err) {
+      alert(`there was an error: ${err}, ${err.message}`);
+    }
+
+  } else if (fileOrFiles.length === 1) {
+
+    const keys = Object.keys(fileOrFiles);
+    keys.forEach(key => {
+      formData.append('files', fileOrFiles[key]);
+    });
+
+    const options = {
+      method: 'POST',
+      body: formData,
+    };
+
+    try {
+      const response = await fetch(`http://127.0.0.1:3001/rename_one/${newName}/${directory}`, options);
+      console.log({ response });
+      $('.success-modal').removeClass('hidden');
+      setTimeout(() => {
+        window.location.reload();
+      }, 1500)
+    } catch (err) {
+      alert(`there was an error: ${err}, ${err.message}`);
+    }
+
+  } else if (fileOrFiles.length > 1) {
+
+    const keys = Object.keys(fileOrFiles);
+    keys.forEach(key => {
+      formData.append('files', fileOrFiles[key]);
+    })
+
+    const options = {
+      method: 'POST',
+      body: formData,
+    };
+
+    try {
+      const response = await fetch(`http://127.0.0.1:3001/rename_multiple/${newName}/${directory}`, options);
+      console.log({ response });
+      $('.success-modal-wrapper').removeClass('hidden');
+      setTimeout(() => {
+        window.location.reload();
+      }, 1500);
+    } catch (err) {
+      alert(`there was an error: ${err}, ${err.message}`);
+    }
+  } else {
+    alert('something isn\'t right . . . make sure you selected files . . .');
+  }
+}
+
+const handleEnterButtonPress = (e) => {
+  if (e.key.toLowerCase() === 'enter') fileDropperLabel.click();
+}
+
+fileDropperLabel.on('dragover', handleFileDrop);
+fileDropperLabel.on('dragenter', handleFileDrop);
+fileDropperLabel.on('drop', handleFileDrop);
+fileDropperLabel.on('keypress', handleEnterButtonPress);
+fileDropperForm.on('submit', handleSubmit);
